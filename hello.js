6346e5fb10c9266f42fc143eeb3ec06973e51697
Spencer Simpson <ssimpson@qualtrics.com>
2020-08-04 08:24:18 -0600

    updated base file

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ var app = express();
 const PORT = 80;
 
 app.get('/', function (req, res) {
-  res.send('Hello World from GCE! Updated using pipeline');
+  res.send('Hello World from GCE! Updated using pipeline, and setting gcloud credentials');
 });
 
 // Comment that is random for updates (one more time)
